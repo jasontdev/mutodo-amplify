e606528106f225947ae290364786e9c99f396a0e
Jason Telfer <jason@jasont.dev>
2021-12-02 12:23:51 +1000

    Minimal/skeleton React Router setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./routes/Welcome";
+
 function App() {
   return (
     <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/routes/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+
+export default function Welcome() {
+    return (
+        <div>
+            <NavBar />
+            <h1>Welcome</h1>
+        </div>
+    )
+}
