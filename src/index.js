43c727a5ab1a9715b2d22d49537d8952a78b7c89
Jason Telfer <jason@jasont.dev>
2021-12-02 21:05:35 +1000

    Altered documment.title from index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+document.title = 'mutodo';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
