ea05107ac7d9b552562388e1fb12a5e41f2770ad
Jason Telfer <jason@jasont.dev>
2021-12-02 21:12:10 +1000

    Added configuration to index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 document.title = 'mutodo';
 
