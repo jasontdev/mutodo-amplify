1a8c7f4fa2b5a88ec45f311e11637755d82d0440
Jason Telfer <jason@jasont.dev>
2021-12-02 20:54:38 +1000

    Enhanced Welcome.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,16 @@
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React from "react";
 
 export default function NavBar() {
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar>
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>Mutodo</Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Mutodo
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
--- src/routes/Welcome.jsx
+++ src/routes/Welcome.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import NavBar from '../components/NavBar';
+import React from "react";
+import { Container, Typography } from "@mui/material";
+import NavBar from "../components/NavBar";
 
 export default function Welcome() {
-    return (
-        <div>
-            <NavBar />
-            <h1>Welcome</h1>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <Container maxWidth="sm">
+        <Typography
+          variant="h3"
+          component="div"
+          sx={{ mt: 5, mb: 3 }}
+          align="center"
+        >
+          Welcome to Mutodo
+        </Typography>
+        <Typography variant="h6" component="div" align="center" sx={{ mb: 3 }}>
+          Multi-user todo application
+        </Typography>
+        <Typography variant="body1" component="div">
+          Mutodo is a product designed to help individuals and organisations
+          colloborate on complicated projects.
+        </Typography>
+      </Container>
+    </div>
+  );
 }
