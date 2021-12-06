7c63e7aa60ea81c541b89b8d0d1430a33431685b
Jason Telfer <jason@jasont.dev>
2021-12-06 11:16:43 +1000

    Updated .gitignore to ignore .idea dir (JetBrains IDEs). Fixed missing dependencies in package.json

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Welcome from "./routes/Welcome";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
 import ConfirmSignup from "./routes/ConfirmSignup";
 import PasswordReset from "./routes/PasswordReset";
 import ConfirmPasswordReset from "./routes/ConfirmPasswordReset";
-import { AuthProvider, RequireAuth } from "./Auth";
+import {AuthProvider, RequireAuth} from "./Auth";
 import Home from "./routes/Home";
 
 function App() {
@@ -14,20 +14,20 @@ function App() {
       <AuthProvider>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Welcome />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/confirm-signup" element={<ConfirmSignup />} />
-            <Route path="/password-reset" element={<PasswordReset />} />
+            <Route path="/" element={<Welcome/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/confirm-signup" element={<ConfirmSignup/>}/>
+            <Route path="/password-reset" element={<PasswordReset/>}/>
             <Route
               path="/confirm-password-reset"
-              element={<ConfirmPasswordReset />}
+              element={<ConfirmPasswordReset/>}
             />
             <Route
               path="/home"
               element={
                 <RequireAuth>
-                  <Home />
+                  <Home/>
                 </RequireAuth>
               }
             />
