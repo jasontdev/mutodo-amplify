894356f4b67a738331dcf29d5ebb26039689dae7
Jason Telfer <jason@jasont.dev>
2021-12-06 13:46:36 +1000

    Added Login, Sign Up and Account buttons to NavBar. Buttons shown according to authentication status.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,37 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import {AccountCircle} from "@mui/icons-material";
+import {useAuth} from "../Auth";
+import {useNavigate} from "react-router";
 
 export default function NavBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mutodo
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const loginAndRegisterButtons = () => {
+    if (auth.user) {
+      return <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+      ><AccountCircle/></IconButton>
+    } else {
+      return (<div>
+        <Button color="inherit" onClick={() => navigate('/login')}>Login</Button>
+        <Button color="inherit" onClick={() => navigate('/signup')}>Sign Up</Button>
+      </div>);
+    }
+  };
+  return (<Box sx={{flexGrow: 1}}>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+          Mutodo
+        </Typography>
+        {loginAndRegisterButtons()}
+      </Toolbar>
+    </AppBar>
+  </Box>);
 }
+
